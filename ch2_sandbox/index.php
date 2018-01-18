dbb6e04abcc61d5710206fa94ddaf7f3841074a1
Leo <leandro.dg@gmail.com>
2018-01-18 10:30:15 -0500

    .

--- ch2_sandbox/index.php
+++ ch2_sandbox/index.php
@@ -13,14 +13,18 @@
  */
 
 get_header(); ?>
-
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
 					<?php
 					if ( have_posts() ):
+
 						while ( have_posts() ) : the_post();
+
+
 							get_template_part( 'template-parts/layout', 'blocks' );
+
 						endwhile;
+
 						the_posts_navigation();
 					endif; ?>
         </main><!-- #main -->
--- ch2_sandbox/template-parts/block-general_copy.php
+++ ch2_sandbox/template-parts/block-general_copy.php
@@ -4,7 +4,7 @@
  */
 ?>
 <section
-        class="block general_copy default-<?php the_sub_field( 'default_background' ); ?> accent-<?php the_sub_field( 'accent_color' ); ?>">
+        class="block general_copy">
     <div class="outer-block-wrapper"> <!-- extend with needed container -->
         <div class="inner-block-wrapper"> <!-- probably extend with row or -->
             <!-- Stuff goes here -->
