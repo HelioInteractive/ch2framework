367268c3ef8d415664bf5ce8ad3c15819e9e7758
Heather Acton <heather@heliointeractive.com>
2018-01-20 22:29:28 -0600

    logo block conditional link around logos, slick images full width, image slider update field names

--- ch2_sandbox/template-parts/block-image_slider.php
+++ ch2_sandbox/template-parts/block-image_slider.php
@@ -6,7 +6,6 @@
 
 wp_enqueue_script( 'slick-slider', get_template_directory_uri() . '/assets/js/slick-min.js', array( jquery ), '1.8', false );
 
-
 wp_enqueue_script( 'slick-fire', get_template_directory_uri() . '/assets/js/slick-fire-min.js', array( jquery ), '1', true );
 
 wp_enqueue_style( 'slick-css', get_template_directory_uri() . '/assets/sass/slick.css' );
@@ -16,24 +15,24 @@ wp_enqueue_style( 'slick-css-theme', get_template_directory_uri() . '/assets/sas
 ?>
 
 <section
-        class="block image_slider default-<?php the_sub_field( 'slider_default_background' ); ?> accent-<?php the_sub_field( 'slider_accent_color' ); ?>">
+        class="block image_slider default-<?php the_sub_field( 'default_background' ); ?> accent-<?php the_sub_field( 'slider_accent_color' ); ?>">
     <div class="outer-block-wrapper"> <!-- extend with needed container -->
         <div class="inner-block-wrapper"> <!-- probably extend with row or -->
             <!-- Stuff goes here -->
             <div class="image_slider--content">
                 <!--follow this pattern for fields, that way their space is not allocated if the field is empty and we do not have weird empty markup everywhere-->
 
-							<?php if ( get_sub_field( 'slider_heading' ) ): ?>
-                  <h3 class="image_slider--title"> <?php the_sub_field( 'slider_heading' ); ?></h3>
+							<?php if ( get_sub_field( 'heading' ) ): ?>
+                  <h3 class="image_slider--title"> <?php the_sub_field( 'heading' ); ?></h3>
 							<?php endif; ?>
 
-							<?php if ( get_sub_field( 'slider_copy' ) ): ?>
-                  <div class="image_slider--copy"> <?php the_sub_field( 'slider_copy' ); ?></div>
+							<?php if ( get_sub_field( 'copy' ) ): ?>
+                  <div class="image_slider--copy"> <?php the_sub_field( 'copy' ); ?></div>
 							<?php endif; ?>
 
-							<?php if ( have_rows( 'slider_slider' ) ) : ?>
+							<?php if ( have_rows( 'slider' ) ) : ?>
                   <div class="slider-wrapper">
-										<?php while ( have_rows( 'slider_slider' ) ) : the_row(); ?>
+										<?php while ( have_rows( 'slider' ) ) : the_row(); ?>
                         <div class="slide">
 													<?php $image = get_sub_field( 'image' ); ?>
 													<?php if ( $image ) { ?>
--- ch2_sandbox/template-parts/block-logo_block.php
+++ ch2_sandbox/template-parts/block-logo_block.php
@@ -11,36 +11,40 @@
             <div class="logo_block--content">
                 <!--follow this pattern for fields, that way their space is not allocated if the field is empty and we do not have weird empty markup everywhere-->
 
-							<?php if ( get_sub_field( 'heading' ) ): ?>
+				<?php if ( get_sub_field( 'heading' ) ): ?>
                   <h3 class="logo_block--title"> <?php the_sub_field( 'heading' ); ?></h3>
-							<?php endif; ?>
+				<?php endif; ?>
 
-							<?php if ( get_sub_field( 'copy' ) ): ?>
+				<?php if ( get_sub_field( 'copy' ) ): ?>
                   <div class="copy"> <?php the_sub_field( 'copy' ); ?></div>
-							<?php endif; ?>
+				<?php endif; ?>
 
 
-							<?php if ( have_rows( 'logos' ) ) : ?>
-								<?php $counter = 0 ?>
-								<?php while ( have_rows( 'logos' ) ) : the_row(); ?>
-									<?php $counter ++; ?>
-								<?php endwhile; ?>
-							<?php endif; ?>
+				<?php if ( have_rows( 'logos' ) ) : ?>
+					<?php $counter = 0 ?>
+					<?php while ( have_rows( 'logos' ) ) : the_row(); ?>
+						<?php $counter ++; ?>
+					<?php endwhile; ?>
+				<?php endif; ?>
 
-							<?php if ( have_rows( 'logos' ) ) : ?>
-                  <div class="logos-wrapper num-<?php echo $counter; ?>">
-										<?php while ( have_rows( 'logos' ) ) : the_row(); ?>
-                        <div class="image-wrapper ">
+				<?php if ( have_rows( 'logos' ) ) : ?>
+                	<div class="logos-wrapper num-<?php echo $counter; ?>">
+					<?php while ( have_rows( 'logos' ) ) : the_row(); ?>
+                		<div class="image-wrapper ">
+	                		<?php if ( get_sub_field( 'link_target' ) ) : ?>
                             <a href="<?php the_sub_field( 'link_target' ); ?>">
-															<?php $logo = get_sub_field( 'logo' ); ?>
-															<?php if ( $logo ) { ?>
+	                        <?php endif; ?>
+								<?php $logo = get_sub_field( 'logo' ); ?>
+								<?php if ( $logo ) { ?>
                                   <img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>"/>
-															<?php } ?>
+								<?php } ?>
+							<?php if ( get_sub_field( 'link_target' ) ) : ?>
                             </a>
+                            <?php endif; ?>
                         </div>
-										<?php endwhile; ?>
+						<?php endwhile; ?>
                   </div>
-							<?php endif; ?>
+				<?php endif; ?>
 
             </div>
         </div>
