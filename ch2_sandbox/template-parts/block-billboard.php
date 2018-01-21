326aed36b4f3dc455f4cc330e3cf66e33371b9d5
Heather Acton <heather@heliointeractive.com>
2018-01-21 14:58:14 -0600

    Remove style choice from billboard - will modify per site

--- ch2_sandbox/template-parts/block-billboard.php
+++ ch2_sandbox/template-parts/block-billboard.php
@@ -6,44 +6,19 @@
  */
 $background_image = get_sub_field( 'background_image' );
 $background       = $background_image['url']; ?>
-<section
-        class="block billboard style-<?php the_sub_field( 'style' ); ?>"
-        style="background-image: url('<?php echo $background; ?>'); background-position: <?php the_sub_field( 'focus' ); ?>;">
-	<?php if ( get_sub_field( 'style' ) == '3' ): ?>
-      <div class="filter"></div>
-	<?php endif; ?>
+<section class="block billboard" style="background-image: url('<?php echo $background; ?>'); background-position: <?php the_sub_field( 'focus' ); ?>;">
+    
+    <div class="filter"></div>
 
-    <div class="outer-block-wrapper"> <!-- extend with needed container -->
-        <div class="inner-block-wrapper"> <!-- probably extend with row or -->
-            <!-- Stuff goes here-->
-
-			<?php if ( ( get_sub_field( 'style' ) == '1' ) || ( get_sub_field( 'style' ) == '2' ) ): ?>
-				<?php if ( get_sub_field( 'heading' ) ): ?>
-					<?php if ( is_front_page() ) : ?>
-						<div class="billboard--title"><h2><?php the_sub_field( 'heading' ); ?></h2></div>
-					<?php else : ?>
-						<div class="billboard--title"><h1><?php the_sub_field( 'heading' ); ?></h1></div>
-					<?php endif; ?>
-				<?php endif; ?>
-			<?php endif; ?>
+    <div class="outer-block-wrapper">
+        <div class="inner-block-wrapper">
 
             <div class="billboard--content">
-                <!--follow this pattern for fields, that way their space is not allocated if the field is empty and we do not have weird empty markup everywhere-->
-				<?php if ( get_sub_field( 'style' ) == '3' ): ?>
-					<?php if ( is_front_page() ) : ?>
-						<h2 class="billboard--title"> <?php the_sub_field( 'heading' ); ?></h2>
-					<?php else : ?>
-						<h1 class="billboard--title"> <?php the_sub_field( 'heading' ); ?></h1>
-					<?php endif; ?>
-				<?php endif; ?>
-
-				<?php if ( get_sub_field( 'style' ) == '1' ): ?>
-					<?php if ( get_sub_field( 'quote_text' ) ): ?>
-                    <div class="quote">
-                    	<blockquote><?php the_sub_field( 'quote_text' ); ?></blockquote>
-                        <!-- ヾ(＾∇＾) Not what the blockquote element is used for. ¯\_(ツ)_/¯-->
-                    </div>
-					<?php endif; ?>
+                
+				<?php if ( is_front_page() ) : ?>
+					<h2 class="billboard--title"> <?php the_sub_field( 'heading' ); ?></h2>
+				<?php else : ?>
+					<h1 class="billboard--title"> <?php the_sub_field( 'heading' ); ?></h1>
 				<?php endif; ?>
 
 				<?php if ( get_sub_field( 'copy' ) ): ?>
@@ -56,4 +31,5 @@ $background       = $background_image['url']; ?>
             </div>
         </div>
     </div>
+    
 </section>
