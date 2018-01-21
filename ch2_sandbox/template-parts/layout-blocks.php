a0c6d6d4adf17914f2ba63079afe87c05d857af0
Leo <leandro.dg@gmail.com>
2018-01-21 12:06:54 -0500

    .cont

--- ch2_sandbox/template-parts/layout-blocks.php
+++ ch2_sandbox/template-parts/layout-blocks.php
@@ -7,7 +7,7 @@ if ( have_rows( 'blocks' ) ) :
 	while ( have_rows( 'blocks' ) ) : the_row();
 
 		$layout = get_row_layout();
-		echo $layout;
+		//echo $layout;
 		get_template_part( 'template-parts/block', $layout );
 
 	endwhile;
