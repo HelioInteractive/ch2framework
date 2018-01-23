58d0b68428c94b4b8266f4a77c0ad14949a24820
Leo <leandro.dg@gmail.com>
2018-01-23 09:42:04 -0500

    .

--- ch2_sandbox/template-parts/block-anchor.php
+++ ch2_sandbox/template-parts/block-anchor.php
@@ -4,10 +4,6 @@
         * Template part for displaying a Anchor.
         */
         ?>
-<section class="block anchor">
-    <div class="outer-block-wrapper"> <!-- extend with needed container -->
-        <div class="inner-block-wrapper"> <!-- probably extend with row or -->
-            <!-- Stuff goes here -->
-        </div>
-    </div>
-</section>
+<a class="block anchor">
+
+</a>
