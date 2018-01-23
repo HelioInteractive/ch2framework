1bcc94297e20b25bc612af70068719320c6386d4
Leo <leandro.dg@gmail.com>
2018-01-22 21:00:57 -0500

    start of anchor block

---
+++ ch2_sandbox/template-parts/block-anchor.php
@@ -0,0 +1,13 @@
+
+		<?php
+        /**
+        * Template part for displaying a Anchor.
+        */
+        ?>
+<section class="block anchor">
+    <div class="outer-block-wrapper"> <!-- extend with needed container -->
+        <div class="inner-block-wrapper"> <!-- probably extend with row or -->
+            <!-- Stuff goes here -->
+        </div>
+    </div>
+</section>
