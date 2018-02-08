e5bd25ed0662d99ce3f6c7bc9045a7a1c201411e
Heather Acton <heather@heliointeractive.com>
2018-02-08 12:11:26 -0600

    change “call to action” block name to “callout”

--- ch2_sandbox/template-parts/block-callout.php
+++ ch2_sandbox/template-parts/block-callout.php
@@ -1,21 +1,21 @@
 <?php
 /**
- * Template part for displaying a Call to action.
+ * Template part for displaying a callout.
  */
 ?>
 <section
-    class="block call_to_action default-<?php the_sub_field( 'default_background' ); ?> style-<?php the_sub_field( 'style' ); ?>"
+    class="block callout default-<?php the_sub_field( 'default_background' ); ?> style-<?php the_sub_field( 'style' ); ?>"
     <?php if ( get_sub_field( 'image' ) ) { ?>
 	    style="background-image: url('<?php echo $background; ?>');"
 	<?php } ?>
 >    <div class="outer-block-wrapper"> <!-- extend with needed container -->
         <div class="inner-block-wrapper"> <!-- probably extend with row or -->
             <!-- Stuff goes here -->
-            <div class="call_to_action--content">
+            <div class="callout--content">
                 <!--follow this pattern for fields, that way their space is not allocated if the field is empty and we do not have weird empty markup everywhere-->
 
 							<?php if ( get_sub_field( 'heading' ) ): ?>
-                  <h3 class="call_to_action--title"> <?php the_sub_field( 'heading' ); ?></h3>
+                  <h3 class="callout--title"> <?php the_sub_field( 'heading' ); ?></h3>
 							<?php endif; ?>
 
 							<?php if ( get_sub_field( 'copy' ) ): ?>
