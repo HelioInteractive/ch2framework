99de9949ab98f6d7868b0490827ca2a9d86a65b6
Leo <leandro.dg@gmail.com>
2018-04-10 13:18:13 -0400

    .

--- ch2_sandbox/header.php
+++ ch2_sandbox/header.php
@@ -21,7 +21,9 @@
 </head>
 
 <body <?php body_class(); ?>>
-
+<script>
+    document.body.classList.add('js');
+</script>
 <div id="page" class="site">
 
     <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ch2_sandbox' ); ?></a>
